bc0dba7a6e046162bc95490c367327aa25492cb9
Marina <mirko_marina@mail.ru>
2018-04-06 21:01:31 +0700

    Добавлен 10 урок

--- lesson_10/js/script.js
+++ lesson_10/js/script.js
@@ -1,27 +1,28 @@
 class options {
 
-			constructor(height, width, bg, fontSize, textAlign) {
-				
-				this.height = height;
-				this.width = width;
-				this.bg = bg;
-				this.fontSize = fontSize;
-				this.textAlign = textAlign;
-			}
-
-			createNewDiv(text) {
-				let div = document.createElement('div');
-				div.textContent = text;
+	constructor(height, width, bg, fontSize, textAlign) {
 		
-				div.style.cssText =
-					`background-color: ${this.bg};
-					text-align: ${this.textAlign}; 
-					width: ${this.width}; 
-					height: ${this.height}; 
-					font-size: ${this.fontSize};`
-					document.body.appendChild(div);
-			}
-		}
+		this.height = height;
+		this.width = width;
+		this.bg = bg;
+		this.fontSize = fontSize;
+		this.textAlign = textAlign;
+	}
 
-		let myObj = new options('200px', '200px', 'red', '20px', 'center');
-		let newDiv = myObj.createNewDiv('Пример текста');
+	createNewDiv(text) {
+		let div = document.createElement('div');
+		div.textContent = text;
+
+		div.style.cssText =
+			`background-color: ${this.bg};
+			text-align: ${this.textAlign}; 
+			width: ${this.width}; 
+			height: ${this.height}; 
+			font-size: ${this.fontSize};`
+			
+		document.body.appendChild(div);
+	}
+}
+
+let myObj = new options('200px', '200px', 'red', '20px', 'center');
+let newDiv = myObj.createNewDiv('Пример текста');
