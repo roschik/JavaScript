570c3f99a6f68ca587cbc5d03beb2c02f6dbd414
Marina <mirko_marina@mail.ru>
2018-06-26 19:23:01 +0700

    Реакт

--- lesson_15/hard/js/num.test.js
+++
@@ -1,5 +0,0 @@
-const num = require('./num');
-
-test('Переменная num равна 5', () => {
-	expect(num).toBe(5);
-});
--- lesson_15/hard/js/sum.test.js
+++
@@ -1,5 +0,0 @@
-const sum = require('./sum');
-
-test('Функция sum() возвращает true', () => {
-	expect(sum(12, 2)).toBe(true);
-});
